1d7a9426c057830bd91b9fa1e3d194354cfdb0e3
Gabriela Schlemper <gabschlemper@gmail.com>
2023-09-29 19:54:17 -0300

    Fix feedback

--- src/app/components/homepage/Feedback.tsx
+++ src/app/components/homepage/Feedback.tsx
@@ -1,7 +1,5 @@
 export function Feedback() {
   return (
-    <div className="h-[1000px] bg-gradient-to-b from-black to-secondary">
-      {/* <h1>Hellowwww World</h1> */}
-    </div>
+    <div className="h-[1000px] bg-gradient-to-b from-black to-secondary"></div>
   );
 }
