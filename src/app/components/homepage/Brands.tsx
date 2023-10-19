8c0b4370ab6e69a10d0e2329520a2ec51808f6de
Gabriela Schlemper <gabschlemper@gmail.com>
2023-10-19 16:45:59 -0300

    Fix responsive brands

--- src/app/components/homepage/Brands.tsx
+++ src/app/components/homepage/Brands.tsx
@@ -4,9 +4,9 @@ import { carouselBrands } from "./data";
 
 export function Brands() {
   return (
-    <ul className="px-4 py-4 lg:px-60 shadow-2xl bg-secondary flex justify-between items-center overflow-scroll w-full gap-5">
+    <ul className="px-4 py-4 lg:px-60 shadow-2xl bg-secondary flex items-center justify-center flex-wrap">
       {carouselBrands.map((item) => (
-        <li key={item.icon} className=" m-auto">
+        <li key={item.icon} className="px-6 py-3">
           <Image src={item.icon} width={40} height={110} alt="" />
         </li>
       ))}
